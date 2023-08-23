4c50fd597e6e2bd3656b2732db23d9825e7d1906
steven <steven.bucaille@gmail.com>
2023-08-23 21:37:50 +0200

    Initial commit with template code generated by transformers-cli

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from . import (
+    superglue,
     albert,
     align,
     altclip,
@@ -201,6 +202,7 @@ from . import (
     regnet,
     rembert,
     resnet,
+    superglue,
     roberta,
     roberta_prelayernorm,
     roc_bert,
