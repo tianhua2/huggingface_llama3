06e25b39da8336236431f433cef9d7b7944b1ae2
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-27 18:47:20 +0200

    fix: remove duplicate line in configuration_auto

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -361,7 +361,6 @@ MODEL_NAMES_MAPPING = OrderedDict(
         ("codegen", "CodeGen"),
         ("cohere", "Cohere"),
         ("colpali", "ColPali"),
-        ("colpali", "ColPali"),
         ("conditional_detr", "Conditional DETR"),
         ("convbert", "ConvBERT"),
         ("convnext", "ConvNeXT"),
